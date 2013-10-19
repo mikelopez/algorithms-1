b2215d46a0b626f106a1f6bc11e91a1e10d0d5af
Mike Lopez <dev@scidentify.info>
2013-10-19 19:42:31 -0400

    performing union

--- lecture__1/dynamic-connectivity-client.cpp
+++ lecture__1/dynamic-connectivity-client.cpp
@@ -16,13 +16,17 @@
 #include <iostream>
 using namespace std;
 
+const int N = 10;
+
 // Quickfind class
 class QuickFindUF {
-    int id[4];
+    int id[N];
     public:
         QuickFindUF(int);
         void custom_union(int, int);
         bool connected(int, int);
+        void print_id();
+        int* get_id() { return id; }
 };
 
 QuickFindUF::QuickFindUF(int N) {
@@ -38,29 +42,62 @@ QuickFindUF::QuickFindUF(int N) {
 
 bool QuickFindUF::connected(int p, int q) {
     // Check whether p and q are in same component
+    // by seeing if the two array values are same ID
     // 2 array accesses
     return id[p] == id[q];
 };
 
 void QuickFindUF::custom_union(int p, int q) {
+    // Custom union function to avoid using union()
     int pid = id[p];
     int qid = id[q];
-    for (int i = 0; i < sizeof(id); i++) {
+    for (int i = 0; i < N; i++) {
         if (id[i] == pid) {
             id[i] = qid;
         }
     }
 };
 
+void print_ids(int id[N]) {
+    cout << "Index\tValue" << endl;
+    for (int i = 0; i < N; i++) {
+        cout << i << "\t" << id[i] << endl;
+    }
+}
 
-
+string askuser() {
+    string result;
+    cout << "What would you like to do?" << endl;
+    cin >> result;
+    return result;
+}
 
 int main ()
 {
-    int N;
-    cout << "Enter a number: ";
-    cin >> N; 
-    cout << "You entered " << N << endl;
+    QuickFindUF uf(N);
+    int *p;
+    string result;
+    p = uf.get_id();
+    cout << "Current Table" << endl;
+    cout << "--------------" << endl;
+
+    print_ids(p);
+
+    result = askuser();
+    while (result != "quit") {
+        if (result == "union") {
+            cout << "Doing union..." << endl;
+            int ux, uy;
+            cout << "select number to union with: " << endl;
+            cin >> ux;
+            cout << "select number to " << ux << " with" << endl;
+            cin >> uy;
+            uf.custom_union(ux, uy);
+            p = uf.get_id();
+            print_ids(p);
+        }
+        result = askuser();
+    }
     return 0;
 
  }
