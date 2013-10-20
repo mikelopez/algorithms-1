0f6d57015b1397608c54277f74f9bfd91d118043
Mike Lopez <dev@scidentify.info>
2013-10-20 00:51:29 -0400

    quickunion in py more verbose

---
+++ lecture-2/python/quickunion.py
@@ -0,0 +1,37 @@
+
+class QuickUnion:
+    id = []
+    def __init__(self):
+        # set value to its index
+        [self.id.append(x) for x in range(0, 10)]
+    
+    def __root(self, n):
+        print "\t- Checking root for %s...."%n
+        while (n != self.id[n]):
+            print "\t\t- n is not id[n] ( %s ) " % self.id[n]
+            n = self.id[n]
+            print "\t\t- n is now set to id[n] ( %s ) " % n
+        return n
+    
+    def union(self, p, q):
+        print "RUNNING UNION(%s, %s) " % (p, q)
+        i = self.__root(p)
+        print "- union() root(p) to i = %s" % i
+        j = self.__root(q)
+        print "- union() root(q) to j = %s" % j
+        self.id[i] = j
+
+        print "set id[i] = j" 
+        print "set id[%s] = %s" % (i, j)
+    
+    def verbose(self):
+        print "Index\tRoot"
+        for index, root in enumerate(self.id):
+            print "%s\t%s" % (index, root)
+
+
+if __name__ == "__main__":
+    qu = QuickUnion()
+    qu.verbose()
+    qu.union(2, 1)
+    qu.verbose()
